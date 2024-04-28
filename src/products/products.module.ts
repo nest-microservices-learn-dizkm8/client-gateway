ae488f9031cbe1a71be4b9811b4b7bfe3afc9202
David Araya Cádiz <david.araya@alumnos.ucn.cl>
2024-04-28 18:33:46 -0400

    feat: config client gateway and basic connection to products ms

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { envs, PRODUCT_SERVICE } from 'src/config';
 import { ProductsController } from './products.controller';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: PRODUCT_SERVICE,
+        transport: Transport.TCP,
+        options: {
+          host: envs.productsMicroserviceHost,
+          port: envs.productsMicroservicePort,
+        },
+      },
+    ]),
+  ],
   controllers: [ProductsController],
 })
 export class ProductsModule {}
