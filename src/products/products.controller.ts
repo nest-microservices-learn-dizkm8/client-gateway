32364da8e1eb95cbb1f9949ba5f4abb139a64ead
David Araya Cádiz <david.araya@alumnos.ucn.cl>
2024-04-28 23:59:30 -0400

    feat: find one and catch errors from products ms

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -4,12 +4,17 @@ import {
   Delete,
   Get,
   Inject,
+  NotFoundException,
   Param,
+  ParseIntPipe,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
 import { PRODUCT_SERVICE } from 'src/config';
+import { PaginationDto } from '../common/dto/pagination.dto';
 
 @Controller('products')
 export class ProductsController {
@@ -23,27 +28,30 @@ export class ProductsController {
   }
 
   @Get()
-  public findAll() {
-    return this.productsClient.send(
-      { cmd: 'find_all_products' },
-      {
-        page: 2,
-      },
-    );
+  public findAll(@Query() pagination: PaginationDto) {
+    return this.productsClient.send({ cmd: 'find_all_products' }, pagination);
   }
 
   @Get(':id')
-  public findOne(@Param('id') id: string) {
-    return `Product with id ${id} fetched`;
+  public async findOne(@Param('id', ParseIntPipe) id: string) {
+    try {
+      const product = await firstValueFrom(
+        this.productsClient.send({ cmd: 'find_one_product' }, { id }),
+      );
+
+      return product;
+    } catch (err) {
+      throw new NotFoundException(err.message);
+    }
   }
 
   @Patch(':id')
-  public update(@Param('id') id: string, @Body() body: any) {
+  public update(@Param('id', ParseIntPipe) id: string, @Body() body: any) {
     return `Product with id ${id} updated`;
   }
 
   @Delete(':id')
-  public delete(@Param('id') id: string) {
+  public delete(@Param('id', ParseIntPipe) id: string) {
     return `Product with id ${id} deleted`;
   }
 }
